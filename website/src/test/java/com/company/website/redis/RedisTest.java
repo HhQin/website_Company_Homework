7d1a859eca792f1d928f55c783fb7557a9959937
HhQin <jeakerqin@163.com>
2018-09-13 13:29:11 +0800

    新增redis依赖

---
+++ website/src/test/java/com/company/website/redis/RedisTest.java
@@ -0,0 +1,14 @@
+package com.company.website.redis;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public class RedisTest {
+
+    @Autowired
+    private
+}
